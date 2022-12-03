420558aac6998a8549ac70d8dfd606a154f89e30
Alex <alexkarpov878@gmail.com>
2022-12-03 11:11:51 -0800

    Create gem with 'bundle gem scrapeful'
    MIT License enabled in config
    Code of conduct enabled in config
    Initializing git repo in /home/alex/Projects/Personal/scrapeful/scrapeful
          create  scrapeful/Gemfile
          create  scrapeful/lib/scrapeful.rb
          create  scrapeful/lib/scrapeful/version.rb
          create  scrapeful/sig/scrapeful.rbs
          create  scrapeful/scrapeful.gemspec
          create  scrapeful/Rakefile
          create  scrapeful/README.md
          create  scrapeful/bin/console
          create  scrapeful/bin/setup
          create  scrapeful/.gitignore
          create  scrapeful/.rspec
          create  scrapeful/spec/spec_helper.rb
          create  scrapeful/spec/scrapeful_spec.rb
          create  scrapeful/.github/workflows/main.yml
          create  scrapeful/LICENSE.txt
          create  scrapeful/CODE_OF_CONDUCT.md
    Gem 'scrapeful' was successfully created. For more information on making a RubyGem visit https://bundler.io/guides/creating_gem.html

---
+++ spec/scrapeful_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Scrapeful do
+  it "has a version number" do
+    expect(Scrapeful::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
